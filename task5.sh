2fbbee7beb33b5d0298491723c04d8bc074f4e48
Viktoriia Karpenko <vkarpenko@griddynamics.com>
2024-10-23 13:57:30 +0300

    Rename reverseFunc to swapFunc. Add new is-statement to this function (task 5)

--- task5.sh
+++ task5.sh
@@ -15,7 +15,7 @@ toLowerCase () {
 }
 
 # function that converts lowercase letters to uppercase and vice versa
-reverseFunc () {
+swapFunc () {
     if [[ $1 =~ [A-Z] ]]; then
         echo $(toLowerCase "$1")
     elif [[ $1 =~ [a-z] ]]; then
@@ -25,38 +25,13 @@ reverseFunc () {
     fi
 }
 
-
 # function that reads a file by symbol
-readByChar () {
+readFile () {
     while IFS= read -r line; do
         new_line=""
-        for (( i=0; i<${#line}; i++ )); do
-            lt="${line:$i:1}"
-            if [ "$1" = "reverse" ]; then
-                new_line+=$(reverseFunc "$lt")
-            elif [ "$1" = "toupper" ]; then
-                new_line+=$(toUpperCase "$lt")
-            elif [ "$1" = "tolower" ]; then
-                new_line+=$(toLowerCase "$lt")
-            fi
-        done
-        new_text+="$new_line\n"
-    done < "$inputfile"
-
-    echo -e "$new_text" > "$outputfile"
-}
-
-
-while [ -n "$1" ]; do
-case "$1" in
-    -i) inputfile="$2"; shift;;
-    -o) outputfile="$2"; shift;;
-    -v) readByChar "reverse"
-        shift;;
-    -s) while IFS= read -r line; do
-            new_line=""
+        if [ "$1" = "replace" ]; then
             for word in $line; do
-                if [[ "${word:0:1}" =~ [Aa] ]]; then
+                if [[ ${word:0:1}"" =~ [Aa] ]; then
                     if [[ "${word:0:1}" = "A" ]]; then
                         new_line+="B${word:1} "
                     else
@@ -66,23 +41,43 @@ case "$1" in
                     new_line+="$word "
                 fi
             done
-            new_text+="$new_line\n"
-        done < "$inputfile"
-        
-        echo -e "$new_text" > "$outputfile"
-        shift;;
-    -r) while IFS= read -r line; do
-            new_line=$(echo "$line" | rev)
-            new_text+="$new_line\n"
-        done < "$inputfile"
+        else
+            for (( i=0; i<${#line}; i++ )); do
+                lt="${line:$i:1}"
+                if [ "$1" = "swap" ]; then
+                    new_line+=$(swapFunc "$lt")
+                elif [ "$1" = "toupper" ]; then
+                    new_line+=$(toUpperCase "$lt")
+                elif [ "$1" = "tolower" ]; then
+                    new_line+=$(toLowerCase "$lt")
+                fi
+            done
+        fi
+        new_text+="$new_line\n"
+    done < "$inputfile"
 
-        echo -e "$new_text" > "$outputfile"
-        shift;;
-    -l) readByChar "tolower"; shift;;
-    -u) readByChar "toupper"; shift;;
-    *) echo "$1 is not an option"; exit 1;;
-esac
-shift
+    echo -e "$new_text" > "$outputfile"
+}
+
+
+while [ -n "$1" ]; do
+    case "$1" in
+        -i) inputfile="$2"; shift;;
+        -o) outputfile="$2"; shift;;
+        -v) readFile "swap"; shift;;
+        -s) readFile "replace"; shift;;
+        -r) while IFS= read -r line; do
+                new_line=$(echo "$line" | rev)
+                new_text+="$new_line\n"
+            done < "$inputfile"
+
+            echo -e "$new_text" > "$outputfile"
+            shift;;
+        -l) readFile "tolower"; shift;;
+        -u) readFile "toupper"; shift;;
+        *) echo "$1 is not an option"; exit 1;;
+    esac
+    shift
 done
 
 
