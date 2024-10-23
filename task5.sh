ae48d250855991ff88beb668040cfa640bf75671
Viktoriia Karpenko <vkarpenko@griddynamics.com>
2024-10-23 12:22:43 +0300

    Add two functions: toUpperCase and toLowerCase in fifth task

--- task5.sh
+++ task5.sh
@@ -4,6 +4,17 @@ inputfile=""
 outputfile=""
 new_text=""
 
+# function that converts lowercase letters to uppercase
+toUpperCase () {
+    echo "$1" | tr 'a-z' 'A-Z'
+}
+
+# function that converts uppercase letters to lowercase
+toLowerCase () {
+    echo "$1" | tr 'A-Z' 'a-z'
+}
+
+
 while [ -n "$1" ]; do
 case "$1" in
     -i) inputfile="$2"; shift;;
@@ -14,9 +25,9 @@ case "$1" in
             for (( i=0; i<${#line}; i++ )); do
                 lt="${line:$i:1}"
                 if [[ $lt =~ [A-Z] ]]; then
-                    new_line+=$(echo "$lt" | tr '[:upper:]' '[:lower:]')
+                    new_line+=$(toLowerCase "$lt")
                 elif [[ $lt =~ [a-z] ]]; then
-                    new_line+=$(echo "$lt" | tr '[:lower:]' '[:upper:]')
+                    new_line+=$(toUpperCase "$lt")
                 else
                     new_line+="$lt"
                 fi
@@ -56,7 +67,7 @@ case "$1" in
             for (( i=0; i<${#line}; i++ )); do
                 lt="${line:$i:1}"
                 if [[ $lt =~ [A-Z] ]]; then
-                    new_line+=$(echo "$lt" | tr '[:upper:]' '[:lower:]')
+                    new_line+=$(toLowerCase "$lt")
                 else
                     new_line+="$lt"
                 fi
@@ -71,7 +82,7 @@ case "$1" in
             for (( i=0; i<${#line}; i++ )); do
                 lt="${line:$i:1}"
                 if [[ $lt =~ [a-z] ]]; then
-                    new_line+=$(echo "$lt" | tr '[:lower:]' '[:upper:]')
+                    new_line+=$(toUpperCase "$lt")
                 else
                     new_line+="$lt"
                 fi
